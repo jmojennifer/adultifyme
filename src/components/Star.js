02c78d044ac01b74dc2ceea371ff6185ed4e019a
Jennifer Owens <jmo.jennifer@gmail.com>
2017-01-04 12:52:07 -0800

    Added Star component/images

---
+++ src/components/Star.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+const Star = () => {
+  return (
+    <Image
+      style={styles.starStyle}
+      source={require('../images/star_gold_32.png')}
+    />
+  );
+};
+
+const styles = {
+  starStyle: {
+    width: 50,
+    height: 50
+  }
+};
+
+export default Star;

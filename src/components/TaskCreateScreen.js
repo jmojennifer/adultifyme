bb911d6993a30cece4fd5f8009e54d58a2f94e6a
Jennifer Owens <jmo.jennifer@gmail.com>
2017-01-08 19:36:06 -0800

    Refactored form fields into new component for DRY reuse

--- src/components/TaskCreateScreen.js
+++ src/components/TaskCreateScreen.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { taskUpdate, taskCreate } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Button } from './common';
+import TaskForm from './TaskForm';
 
 class TaskCreateScreen extends Component {
   onButtonPress() {
@@ -24,70 +24,7 @@ class TaskCreateScreen extends Component {
   render() {
     return (
       <Card>
-        <CardSection>
-          <Input
-            label="Title"
-            placeholder="i.e. Take Vitamin C"
-            value={this.props.title}
-            onChangeText={text => this.props.taskUpdate({ prop: 'title', value: text })
-            }
-          />
-        </CardSection>
-
-        <CardSection>
-          <Input
-            label="Description"
-            placeholder="Take 2 pills each evening with a full glass of water"
-            value={this.props.description}
-            onChangeText={text => this.props.taskUpdate({ prop: 'description', value: text })}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Input
-            label="Personal Motivation"
-            placeholder="i.e. Let's not get sick!"
-            value={this.props.personalMotivation}
-            onChangeText={text => this.props.taskUpdate({ prop: 'personalMotivation', value: text })}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Text style={styles.pickerTextStyle}>Category</Text>
-          <Picker
-            style={styles.pickerStyle}
-            selectedValue={this.props.category}
-            onValueChange={selection => this.props.taskUpdate({ prop: 'category', value: selection })}
-          >
-            <Picker.Item label="Finance" value="Finance" />
-            <Picker.Item label="Health/Medical" value="Health/Medical" />
-            <Picker.Item label="Fitness" value="Fitness" />
-            <Picker.Item label="Career" value="Career" />
-            <Picker.Item label="Personal Wellness" value="Personal Wellness" />
-            <Picker.Item label="Household" value="Household" />
-            <Picker.Item label="Social" value="Social" />
-            <Picker.Item label="Other" value="Other" />
-          </Picker>
-        </CardSection>
-
-        <CardSection>
-          <Input
-            label="Due Date"
-            placeholder="i.e. 1/27/2017"
-            value={this.props.dueDate}
-            onChangeText={text => this.props.taskUpdate({ prop: 'dueDate', value: text })}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Input
-            label="Time Due"
-            placeholder="5:00PM"
-            value={this.props.timeDue}
-            onChangeText={text => this.props.taskUpdate({ prop: 'timeDue', value: text })}
-          />
-        </CardSection>
-
+        <TaskForm {...this.props} />
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Create
@@ -97,19 +34,6 @@ class TaskCreateScreen extends Component {
     );
   }
 }
-
-const styles = {
-  pickerTextStyle: {
-    fontSize: 18,
-    marginLeft: 20,
-    marginTop: 10
-  },
-  pickerStyle: {
-    flex: 1,
-    marginHorizontal: 25
-  }
-};
-
 
 const mapStateToProps = (state) => {
   const { title, description, personalMotivation, category, dueDate, timeDue } = state.taskForm;
---
+++ src/components/TaskForm.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { View, Picker, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { taskUpdate } from '../actions';
+import { CardSection, Input } from './common';
+
+class TaskForm extends Component {
+  render() {
+    return (
+      <View>
+      <CardSection>
+        <Input
+          label="Title"
+          placeholder="i.e. Take Vitamin C"
+          value={this.props.title}
+          onChangeText={text => this.props.taskUpdate({ prop: 'title', value: text })
+          }
+        />
+      </CardSection>
+
+      <CardSection>
+        <Input
+          label="Description"
+          placeholder="Take 2 pills each evening with a full glass of water"
+          value={this.props.description}
+          onChangeText={text => this.props.taskUpdate({ prop: 'description', value: text })}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Input
+          label="Personal Motivation"
+          placeholder="i.e. Let's not get sick!"
+          value={this.props.personalMotivation}
+          onChangeText={text => this.props.taskUpdate({ prop: 'personalMotivation', value: text })}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Text style={styles.pickerTextStyle}>Category</Text>
+        <Picker
+          style={styles.pickerStyle}
+          selectedValue={this.props.category}
+          onValueChange={selection => this.props.taskUpdate({ prop: 'category', value: selection })}
+        >
+          <Picker.Item label="Finance" value="Finance" />
+          <Picker.Item label="Health/Medical" value="Health/Medical" />
+          <Picker.Item label="Fitness" value="Fitness" />
+          <Picker.Item label="Career" value="Career" />
+          <Picker.Item label="Personal Wellness" value="Personal Wellness" />
+          <Picker.Item label="Household" value="Household" />
+          <Picker.Item label="Social" value="Social" />
+          <Picker.Item label="Other" value="Other" />
+        </Picker>
+      </CardSection>
+
+      <CardSection>
+        <Input
+          label="Due Date"
+          placeholder="i.e. 1/27/2017"
+          value={this.props.dueDate}
+          onChangeText={text => this.props.taskUpdate({ prop: 'dueDate', value: text })}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Input
+          label="Time Due"
+          placeholder="5:00PM"
+          value={this.props.timeDue}
+          onChangeText={text => this.props.taskUpdate({ prop: 'timeDue', value: text })}
+        />
+      </CardSection>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    marginLeft: 20,
+    marginTop: 10
+  },
+  pickerStyle: {
+    flex: 1,
+    marginHorizontal: 25
+  }
+};
+
+const mapStateToProps = (state) => {
+  const { title, description, personalMotivation, category, dueDate, timeDue } = state.taskForm;
+
+  return { title, description, personalMotivation, category, dueDate, timeDue };
+};
+
+export default connect(mapStateToProps, { taskUpdate })(TaskForm);

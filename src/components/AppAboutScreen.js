37f34da95df83781eba93b48c86a05f78a7df255
Jennifer Owens <jmo.jennifer@gmail.com>
2017-01-25 10:47:49 -0800

    Added content to About screen

--- src/components/AppAboutScreen.js
+++ src/components/AppAboutScreen.js
@@ -5,6 +5,32 @@ import { Text, View, Linking } from 'react-native';
 const AppAboutScreen = () => {
   return (
     <View style={styles.containerStyle}>
+      <Text style={styles.headerStyle}>
+        App
+      </Text>
+      <Text
+        style={styles.textStyle}
+      >
+        Created as Jennifer Owens’ Ada Developers Academy solo capstone project, this app is the culmination of 5 months of classroom instruction and 1 month of development time. It is intended to showcase skills learned via self-study, use of new languages and concepts, and follow-through by completing an independently planned product using sound project management principles.
+      </Text>
+      <Text
+        style={styles.linkStyle}
+        onPress={() => Linking.openURL('http://adadevelopersacademy.org/')}
+      >
+          Ada Developers Academy website
+      </Text>
+      <Text
+        style={styles.linkStyle}
+        onPress={() => Linking.openURL('https://github.com/jmojennifer/adultifyme')}
+      >
+        Github repo
+      </Text>
+      <View style={styles.containerStyle}>
+        <Text
+          style={styles.headerStyle}
+        >
+          Credits
+        </Text>
         <Text
           style={styles.textStyle}
         >
@@ -22,23 +48,35 @@ const AppAboutScreen = () => {
         >
           Delete icon
         </Text>
+      </View>
     </View>
   );
 };
 
 const styles = {
   containerStyle: {
-    alignItems: 'center'
+    alignItems: 'center',
+
   },
-  textStyle: {
+  headerStyle: {
     fontWeight: 'bold',
     fontSize: 18,
     paddingBottom: 5
   },
+  textStyle: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center',
+    paddingBottom: 10,
+    paddingLeft: 30,
+    paddingRight: 30
+  },
   linkStyle: {
     color: '#22333B',
     fontWeight: 'bold',
-    fontSize: 18
+    textAlign: 'justify',
+    fontSize: 15,
+    paddingBottom: 10
   }
 };
 
